e138386fed9d17928b1b74df2338a21f1cb938d1
KILLER04-star <paulneumann@gmail.com>
2020-11-24 19:25:55 +0100

    fixed layout, added keylistener

--- src/GUI.java
+++ src/GUI.java
@@ -8,16 +8,18 @@ public class GUI
 {
     public static JFrame mainFrame = new JFrame("Fahrkarten");
     public static JPanel mainPanel = new JPanel();
-    public static JTextField DestinationTV = new JTextField("Ziel");
-    public static JTextField startTextField = new JTextField("Start");
+    public static JTextField DestinationTV = new JTextField();
+    public static JTextField startTextField = new JTextField();
     public static ArrayList<JTextField> possible_station = new ArrayList<>();
     public static  JTextField ErrorTV = new JTextField();
-    
+    public static JLabel startLabel = new JLabel("von");
+    public static JLabel toJLabel = new JLabel("nach");
+
     public static int possible_station_x = 350;
     public static int possible_station_y = 50;
 
     public static int size = 0;
-    
+
     public static void createGUI()
     {
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
@@ -41,6 +43,7 @@ public class GUI
         {
             ErrorTV.setText("Fehler: Suche für "+Query+" ergab 0 Ergebnisse");
             ErrorTV.setBounds(possible_station_x,possible_station_y,300,30);
+            ErrorTV.setBounds(TV.getX(),possible_station_y+10+startTextField.getHeight(),300,30);
             ErrorTV.setVisible(true);
             ErrorTV.setEnabled(true);
             ErrorTV.setEditable(false);
@@ -58,35 +61,36 @@ public class GUI
         if(size>20) size= 20;
         for(int i = 0;i<size;i++)
         {
-           JTextField textField = new JTextField(list.get(i).getName());
-           textField.setBounds(possible_station_x,possible_station_y,300,30);
-           textField.setVisible(true);
-           textField.setEnabled(true);
-           textField.setEditable(false);
-           possible_station_y+=40;
-           mainPanel.add(textField);
-           mainFrame.add(mainPanel);
-           paint();
-           textField.addMouseListener(new MouseAdapter()
-           {
-               @Override
-               public void mouseClicked(MouseEvent e)
-               {
-                   if(textField.getText().equals(OtherTV.getText()))
-                   {
-                       same_station_error();
-                   }else
-                       {
-                       TV.setText(textField.getText());
-                           possible_station.removeAll(collection);
-                           mainPanel.removeAll();
-                           mainFrame.add(mainPanel);
-                           mainFrame.repaint();
-                           paint();
-                   }
-               }
-           });
-           possible_station.add(textField);
+            JTextField textField = new JTextField(list.get(i).getName());
+            textField.setBounds(possible_station_x,possible_station_y,300,30);
+            textField.setBounds(TV.getX(),possible_station_y+TV.getHeight()+10,300,30);
+            textField.setVisible(true);
+            textField.setEnabled(true);
+            textField.setEditable(false);
+            possible_station_y+=40;
+            mainPanel.add(textField);
+            mainFrame.add(mainPanel);
+            paint();
+            textField.addMouseListener(new MouseAdapter()
+            {
+                @Override
+                public void mouseClicked(MouseEvent e)
+                {
+                    if(textField.getText().equals(OtherTV.getText()))
+                    {
+                        same_station_error();
+                    }else
+                    {
+                        TV.setText(textField.getText());
+                        possible_station.removeAll(collection);
+                        mainPanel.removeAll();
+                        mainFrame.add(mainPanel);
+                        mainFrame.repaint();
+                        paint();
+                    }
+                }
+            });
+            possible_station.add(textField);
         }
         possible_station_y = 50;
     }
@@ -103,6 +107,17 @@ public class GUI
         mainPanel.add(startTextField);
 
         DestinationTV.setBounds(800,50,300,30);
+        startLabel.setVisible(true);
+        startLabel.setEnabled(true);
+        startLabel.setBounds(50,25,300,25);
+        mainPanel.add(startLabel);
+
+        toJLabel.setBounds(startTextField.getX()+startTextField.getWidth()+50,25,300,25);
+        toJLabel.setVisible(true);
+        toJLabel.setEnabled(true);
+        mainPanel.add(toJLabel);
+
+        DestinationTV.setBounds(startTextField.getX()+startTextField.getWidth()+50,50,300,30);
         DestinationTV.setEnabled(true);
         DestinationTV.setEditable(true);
         DestinationTV.setVisible(true);

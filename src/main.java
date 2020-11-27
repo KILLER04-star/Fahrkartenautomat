a988a09ea9200ec393de0b7466744680f07ab0ac
KILLER04-star <paulneumann@gmail.com>
2020-11-27 12:31:52 +0100

    refactor

--- src/main.java
+++ src/main.java
@@ -1,10 +1,7 @@
-@SuppressWarnings("ALL")
 public class main
 {
     public static void main(String[] args)
     {
-      //  ExcelReader reader = new ExcelReader("src/DB.csv");
-      //  reader.read();
         GUI.createGUI();
         ActionListener.Actions();
     }

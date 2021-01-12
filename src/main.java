94ef5931cf8249227d74d3ea807597de1e477e8d
KILLER04-star <paulneumann@gmail.com>
2021-01-12 17:37:51 +0100

    cleanup

--- src/main.java
+++ src/main.java
@@ -1,3 +1,7 @@
+/*
+Aufgrund von verwendeten Bibliotheken kann es sein, dass das Programm im Java-Editor nicht funktioniert.
+Weiterhin wird openjdk Version 15 verwendet.
+ */
 public class main //Bitte diese Klasse als Hauptklasse ausführen
 {
     public static void main(String[] args) //Hauptklasse

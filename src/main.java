473fdf7c55fab7f5094a60f4e47a83654423e1ed
KILLER04-star <paulneumann@gmail.com>
2021-01-12 17:34:40 +0100

    Cleanup

--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ public class main //Bitte diese Klasse als Hauptklasse ausführen
 {
     public static void main(String[] args) //Hauptklasse
     {
+        System.out.println("Code unter: https://github.com/KILLER04-star/Fahrkartenautomat");
         GUI.createGUI();
         ActionListener.Actions();
     }
